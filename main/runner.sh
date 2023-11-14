d06cc5a4b328b344e1ed2f9b3f99376dfbd8ad1f
SMahdiHosseini <shossein@lpdpc28.epfl.ch>
2023-11-14 18:12:40 +0100

    new run results added!

--- main/runner.sh
+++ main/runner.sh
@@ -42,7 +42,7 @@ for agg in ${aggregator[@]}; do
             mkdir -p "../FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy/"$att"
             echo running FL with "$nb_byz" byzantine client. Accuracy test phase! Aggregator: "$agg" Attack: "$att"
             ((new_nb_clients = nb_clients - nb_byz))
-            bash FLRunner.sh "$new_nb_clients" "$localHost" "$server_port" "$nb_byz" "$rounds" "$agg" "$att" "accuracy"
+            # bash FLRunner.sh "$new_nb_clients" "$localHost" "$server_port" "$nb_byz" "$rounds" "$agg" "$att" "accuracy"
         done
     fi
 
@@ -57,7 +57,7 @@ for agg in ${aggregator[@]}; do
             mkdir -p "../Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy/"$att"
             echo running p2p with "$nb_byz" byzantine client. Accuracy test phase! Aggregator: "$agg" Attack: "$att"
             ((new_nb_clients = nb_clients - nb_byz))
-            bash gossipRunner.sh "$new_nb_clients" "$localHost" "$server_port" "$nb_byz" "$rounds" "$agg" "$att" "accuracy"
+            # bash gossipRunner.sh "$new_nb_clients" "$localHost" "$server_port" "$nb_byz" "$rounds" "$agg" "$att" "accuracy"
         done
     fi
 
