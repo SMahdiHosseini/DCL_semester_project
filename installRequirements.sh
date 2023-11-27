ca1e669c2fd9875448fcdd23f5f53a1bb5ad05ad
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-11-27 18:18:57 +0100

    automted!

--- installRequirements.sh
+++ installRequirements.sh
@@ -1,11 +1,12 @@
-cd ../
-git clone https://github.com/Blockchain-Benchmarking/silk.git
-cd silk
-sudo wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-export PATH=$PATH:/usr/local/go/bin
-source ~/.profile
-make all
+# git clone https://github.com/Blockchain-Benchmarking/silk.git
+# cd silk
+# sudo wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+# export PATH=$PATH:/usr/local/go/bin
+# source ~/.profile
+# make all
 pip install torch
 pip install jpysocket
 pip install numpy
 pip install torchvision
+# cd bin
+# ./silk server
---
+++ temp.sh
@@ -0,0 +1,3 @@
+server=51.20.95.91
+
+git clone https://github.com/Blockchain-Benchmarking/silk.git
