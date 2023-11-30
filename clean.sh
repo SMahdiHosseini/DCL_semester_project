e07d587fe2b25c8840983ee96a310e4a5903d351
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-11-30 16:46:36 +0100

    To test1

--- clean.sh
+++ clean.sh
@@ -1,10 +1,27 @@
+# . global.config
+
+
+# ssh ubuntu@"$server" 'rm -rf DCL_semester_project' &
+# for client in "${clients[@]}"
+# do
+#     ssh ubuntu@"$client" 'rm -rf DCL_semester_project' &
+# done
+
+# wait
+
+# echo "*********************"
+# echo "*********************" 
+# echo "       Done!         "
+# echo "*********************"
+# echo "*********************"
+
 . global.config
 
 
-ssh ubuntu@"$server" 'rm -rf DCL_semester_project' &
+ssh ubuntu@"$server" 'killall python3' &
 for client in "${clients[@]}"
 do
-    ssh ubuntu@"$client" 'rm -rf DCL_semester_project' &
+    ssh ubuntu@"$client" 'killall python3' &
 done
 
 wait
