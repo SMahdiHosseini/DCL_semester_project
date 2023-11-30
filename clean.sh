519e6538ebd1cb35256ff9ce991a3d8ba3eafba5
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-11-30 17:34:57 +0100

    P2P senarios also works and is fully automated!

--- clean.sh
+++ clean.sh
@@ -1,27 +1,10 @@
-. global.config
-
-
-ssh ubuntu@"$server" 'rm -rf DCL_semester_project' &
-for client in "${clients[@]}"
-do
-    ssh ubuntu@"$client" 'rm -rf DCL_semester_project' &
-done
-
-wait
-
-echo "*********************"
-echo "*********************" 
-echo "       Done!         "
-echo "*********************"
-echo "*********************"
-
 # . global.config
 
 
-# ssh ubuntu@"$server" 'killall python3' &
+# ssh ubuntu@"$server" 'rm -rf DCL_semester_project' &
 # for client in "${clients[@]}"
 # do
-#     ssh ubuntu@"$client" 'killall python3' &
+#     ssh ubuntu@"$client" 'rm -rf DCL_semester_project' &
 # done
 
 # wait
@@ -31,3 +14,20 @@ echo "*********************"
 # echo "       Done!         "
 # echo "*********************"
 # echo "*********************"
+
+. global.config
+
+
+ssh ubuntu@"$server" 'killall python3' &
+for client in "${clients[@]}"
+do
+    ssh ubuntu@"$client" 'killall python3' &
+done
+
+wait
+
+echo "*********************"
+echo "*********************" 
+echo "       Done!         "
+echo "*********************"
+echo "*********************"
