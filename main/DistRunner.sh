3015863bddab6d53d724ed6592abd3495035f0b2
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-11-30 15:50:57 +0100

    To test1

--- main/DistRunner.sh
+++ main/DistRunner.sh
@@ -63,7 +63,7 @@ then
     mkdir -p "../Gossip_res/""$3"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance
     mkdir -p "../Gossip_res/""$3"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
     echo running p2p client "$5" with "$nb_byz" byzantine client. Performanace test phase! Aggregator: "$3"
-    python3 Client_Gossip.py $5 $nb_clients "0.0.0.0" "$server_port" "$nb_byz" "$nb_rounds" "$3" "att "Performance > "./Results/gossip/res_$5.txt"
+    python3 Client_Gossip.py "$5" "$nb_clients" "0.0.0.0" "$server_port" "$nb_byz" "$nb_rounds" "$3" "att" "Performance" > "./Results/gossip/res_$5.txt"
 fi
 
 # if [[ "$1" == "con" || "$1" == "all" ]]
