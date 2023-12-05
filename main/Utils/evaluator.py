e1c56b1b2412561d4bdb3c10c2a10b9728a7b5e5
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-12-05 14:57:41 +0100

    Modified for Accuracy test!

--- main/Utils/evaluator.py
+++ main/Utils/evaluator.py
@@ -6,15 +6,16 @@ def evaluateTheRound(params, r, text_file):
     # train_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/trainDataset.pt")
     # test_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/testDataset.pt")
     # dev_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/devDataset.pt")
-    train_dataset = load("Data/trainDataset.pt")
+    # train_dataset = load("Data/trainDataset.pt")
     test_dataset = load("Data/testDataset.pt")
-    dev_dataset = load("Data/devDataset.pt")
+    # dev_dataset = load("Data/devDataset.pt")
 
     global_net = Helper.to_device(Model.FederatedNet(), Helper.device)
     global_net.apply_parameters(params)
-    train_loss, train_acc = global_net.evaluate(train_dataset)
-    dev_loss, dev_acc = global_net.evaluate(dev_dataset)
+    # train_loss, train_acc = global_net.evaluate(train_dataset)
+    # dev_loss, dev_acc = global_net.evaluate(dev_dataset)
     test_loss, test_acc = global_net.evaluate(test_dataset)
-    text_file.write('After round {}, train_loss = {}, train_acc = {}, dev_loss = {}, dev_acc = {}, test_loss = {}, test_acc = {}\n'.format(r, round(train_loss, 4), round(train_acc, 4), round(dev_loss, 4), round(dev_acc, 4), round(test_loss, 4), round(test_acc, 4)))
+    # text_file.write('After round {}, train_loss = {}, train_acc = {}, dev_loss = {}, dev_acc = {}, test_loss = {}, test_acc = {}\n'.format(r, round(train_loss, 4), round(train_acc, 4), round(dev_loss, 4), round(dev_acc, 4), round(test_loss, 4), round(test_acc, 4)))
+    text_file.write('After round {}, test_loss = {}, test_acc = {}\n'.format(r, round(test_loss, 4), round(test_acc, 4)))
 
     
