fa6cc7ddc3174d2363126cee1c6d96ee8c49ac6e
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-12-05 14:49:46 +0100

    Modified for Accuracy test!

--- main/Utils/evaluator.py
+++ main/Utils/evaluator.py
@@ -6,9 +6,9 @@ def evaluateTheRound(params, r, text_file):
     # train_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/trainDataset.pt")
     # test_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/testDataset.pt")
     # dev_dataset = load("F:/DCL/Semester Project 1/Codes/DCL_semester_project/main/Data/devDataset.pt")
-    train_dataset = load("/localhome/shossein/DCL_semester_project/main/Data/trainDataset.pt")
-    test_dataset = load("/localhome/shossein/DCL_semester_project/main/Data/testDataset.pt")
-    dev_dataset = load("/localhome/shossein/DCL_semester_project/main/Data/devDataset.pt")
+    train_dataset = load("Data/trainDataset.pt")
+    test_dataset = load("Data/testDataset.pt")
+    dev_dataset = load("Data/devDataset.pt")
 
     global_net = Helper.to_device(Model.FederatedNet(), Helper.device)
     global_net.apply_parameters(params)
