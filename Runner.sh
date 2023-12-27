99e419bb88cb480a6fcc43bb4777bd3b1e2a9193
SMahdiHosseini <smahdihosseini4@gmail.com>
2023-12-27 16:01:52 +0100

    Random Latency added to the system1

--- Runner.sh
+++ Runner.sh
@@ -1,3 +1,92 @@
+function add_latency {
+    echo "Adding latency to the network..."
+    i=0
+    d=1
+    for client in "${clients[@]}"
+    do
+        r=$(cat latency.txt | grep "client_$i" | cut -d'=' -f2)
+        ssh ubuntu@"$client" 'sudo tc qdisc add dev ens5 root netem delay '"$r"'ms'
+        i=$(( $i + $d ))
+    done
+}
+
+function remove_latency {
+    echo "Removing latency from the network..."
+    i=0
+    d=1
+    for client in "${clients[@]}"
+    do
+        ssh ubuntu@"$client" 'sudo tc qdisc del dev ens5 root 2>/dev/null'
+        i=$(( $i + $d ))
+    done
+}
+
+function transfer_files {
+    if [[ "$1" == "fl" || "$1" == "all" ]]
+    then
+        mkdir -p "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
+        rm -rf "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
+        # mkdir -p "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
+
+        scp ubuntu@$server:/home/ubuntu/DCL_semester_project/FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/server.txt  ./FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+        scp ubuntu@$server:/home/ubuntu/DCL_semester_project/main/Results/FL/server_res.txt  ./main/Results/FL &
+        i=0
+        d=1
+        for client in "${clients[@]}"
+        do
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/main/Results/FL/res_$i.txt  ./main/Results/FL &
+            i=$(( $i + $d ))
+        done  
+    fi
+
+    if [[ "$1" == "p2p" || "$1" == "all" ]]
+    then
+        mkdir -p "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
+        rm -rf "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
+        # mkdir -p "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
+        i=0
+        d=1
+        for client in "${clients[@]}"
+        do
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Gossip_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./Gossip_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/main/Results/gossip/res_$i.txt  ./main/Results/gossip &
+            i=$(( $i + $d ))
+        done  
+    fi
+
+    if [[ "$1" == "con" || "$1" == "all" ]]
+    then
+        mkdir -p "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
+        rm -rf "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
+        # mkdir -p "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
+        i=0
+        d=1
+        for client in "${clients[@]}"
+        do
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/server_$i.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+            scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/recevdParamIds.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
+            i=$(( $i + $d ))
+        done  
+    fi
+}
+
+function read_bytes {
+    recvd=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/rx_bytes')
+    send=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/tx_bytes')
+    echo "server=$recvd, $send"
+    i=0
+    d=1
+    for client in "${clients[@]}"
+    do
+        recvd=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/rx_bytes')
+        send=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/tx_bytes')
+        echo "client_$i=$recvd, $send"
+        i=$(( $i + $d ))
+    done
+}
+
 if [ $# -lt 2 ]
 then
     echo "usage: $0 <senario> <config>"
@@ -25,21 +114,12 @@ fi
 . global.config
 
 ## Performance Test
-for j in {3..3}
+for j in {1..1}
 do
     for agg in ${aggregator[@]}; do
-        recvd=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/rx_bytes')
-        send=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/tx_bytes')
-        echo "server=$recvd, $send" > main/net_$1.txt
-        i=0
-        d=1
-        for client in "${clients[@]}"
-        do
-            recvd=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/rx_bytes')
-            send=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/tx_bytes')
-            echo "client_$i=$recvd, $send" >> main/net_$1.txt
-            i=$(( $i + $d ))
-        done
+        read_bytes > main/net_$1.txt
+
+        add_latency
 
         if [[ "$1" == "fl" ]]
         then
@@ -76,18 +156,9 @@ do
         sleep 40s
         # wait
 
-        recvd=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/rx_bytes')
-        send=$(ssh ubuntu@"$server" 'cat /sys/class/net/ens5/statistics/tx_bytes')
-        echo "server=$recvd, $send" >> main/net_$1.txt
-        i=0
-        d=1
-        for client in "${clients[@]}"
-        do
-            recvd=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/rx_bytes')
-            send=$(ssh ubuntu@"$client" 'cat /sys/class/net/ens5/statistics/tx_bytes')
-            echo "client_$i=$recvd, $send" >> main/net_$1.txt
-            i=$(( $i + $d ))
-        done
+        remove_latency
+
+        read_bytes >> main/net_$1.txt
 
         bash Terminate.sh
 
@@ -96,54 +167,9 @@ do
         echo "     $agg Done!      "
         echo "*********************"
         echo "*********************"
-        if [[ "$1" == "fl" || "$1" == "all" ]]
-        then
-            mkdir -p "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
-            rm -rf "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
-            # mkdir -p "./FL_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
 
-            scp ubuntu@$server:/home/ubuntu/DCL_semester_project/FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/server.txt  ./FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-            scp ubuntu@$server:/home/ubuntu/DCL_semester_project/main/Results/FL/server_res.txt  ./main/Results/FL &
-            i=0
-            d=1
-            for client in "${clients[@]}"
-            do
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./FL_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/main/Results/FL/res_$i.txt  ./main/Results/FL &
-                i=$(( $i + $d ))
-            done  
-        fi
+        transfer_files $1 $agg $j
 
-        if [[ "$1" == "p2p" || "$1" == "all" ]]
-        then
-            mkdir -p "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
-            rm -rf "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
-            # mkdir -p "./Gossip_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
-            i=0
-            d=1
-            for client in "${clients[@]}"
-            do
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Gossip_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./Gossip_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/main/Results/gossip/res_$i.txt  ./main/Results/gossip &
-                i=$(( $i + $d ))
-            done  
-        fi
-
-        if [[ "$1" == "con" || "$1" == "all" ]]
-        then
-            mkdir -p "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j
-            rm -rf "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Performance_$j/*
-            # mkdir -p "./Consensus_res/""$agg"/ncl_"$nb_clients"/nbyz_"$nb_byz"/Accuracy
-            i=0
-            d=1
-            for client in "${clients[@]}"
-            do
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/$i.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/server_$i.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-                scp ubuntu@$client:/home/ubuntu/DCL_semester_project/Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance/recevdParamIds.txt  ./Consensus_res/$agg/ncl_$nb_clients/nbyz_$nb_byz/Performance_$j &
-                i=$(( $i + $d ))
-            done  
-        fi
         wait
         echo "*********************"
         echo "*********************" 
